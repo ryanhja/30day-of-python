7a364a0ceec812a4de008748cd768823b2cf5939
ryanhja <rayanstive9@gmail.com>
2026-01-08 22:47:34 +0300

    feat(30DaysOfPython): Add day 1

---
+++ 30DaysOfPython/day_1/helloworld.py
@@ -0,0 +1,26 @@
+#1 Operation
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#2 String
+print("Rayan")
+print("HAJANANTENAINA")
+print("Madagascar")
+print("I am enjoying 30 days of python")
+
+#3 Data type
+print(type(10))                                    #Int
+print(type(3.14))                                  #Float
+print(type(4 - 4j))                                #Complex
+print(type("Rayan"))                               #Str
+print(type(['Rayan', 'Python', 'Madagascar']))     #List
+print(type({'name': 'Rayan'}))                     #Dict
+print(type({9.8, 3.14, 5.2}))                      #Set
+print(type((9.8, 3.14, 5.2)))                      #Tuple
+print(type(3 == 3))                                #Bool
+
