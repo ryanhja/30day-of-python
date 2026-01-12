18b5053e2e148ca46461415f799f0b096cd3bfb7
ryanhja <rayanstive9@gmail.com>
2026-01-12 22:09:57 +0300

    feat(30DaysOfPython): Add day 4

---
+++ 30DaysOfPython/day_4/string.py
@@ -0,0 +1,29 @@
+# Day 4: 30 Days of python programming
+
+print("Exercise 01:")
+str_1 = "Thirty"
+str_2 = "Days"
+str_3 = "Of"
+str_4 = "Python"
+space = " "
+result_01 = str_1 + space + str_2 + space + str_3 + space + str_4
+print(result_01)
+
+print("\nExercise 02:")
+result_02 =  'Coding' + ' ' + 'For' + ' ' + 'All'
+print(result_02)
+
+print("\nExercise 03:")
+company = "Coding For All"
+
+print("\nExercise 04:")
+print(company)
+
+print("\nExercise 05:")
+print(len(company))
+
+print("\nExercise 06:")
+print(company.upper())
+
+print("\nExercise 07:")
+print(company.lower())
