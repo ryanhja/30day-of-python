c98d30a906fa27d2dd660ca79c2405247d7bf68b
ryanhja <rayanstive9@gmail.com>
2026-01-16 22:28:52 +0300

    feat(30DaysOfPython): Add day 5

---
+++ 30DaysOfPython/day_5/list.py
@@ -0,0 +1,22 @@
+# Day 5: 30 Days of python programming
+lst = []
+print("Empty list : ",lst)
+
+fruits = ['banana', 'orange', 'mango', 'lemon', "apple"]
+print("List with 5 items : ",fruits)
+
+print("Length list : ",len(fruits))
+
+print(f"First item = {fruits[0]}, Middle item = {fruits[int(len(fruits)/2)]}, Last item = {fruits[-1]}")
+
+mixed_data_types = ["Rayan", 26, 72, "Single", {"Country": "Madagascar", "City": "Antananarivo"}]
+print(mixed_data_types)
+
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+print(it_companies)
+
+print(len(it_companies))
+
+it_companies.append('Samsung')
+print(it_companies)
