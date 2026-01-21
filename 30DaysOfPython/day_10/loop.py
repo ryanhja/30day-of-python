dc04a9d7dd187d09b5ae5085fe3d8e0124be78b6
ryanhja <rayanstive9@gmail.com>
2026-01-21 22:54:15 +0300

    feat(30DaysOfPython): Add day 10

--- 30DaysOfPython/day_10/loop.py
+++ 30DaysOfPython/day_10/loop.py
@@ -1,27 +1,277 @@
 # Day 10: 30 Days of python programming
-
+from countries import countries_list
 print("Loop for")
-for i in range (10):
+for i in range(10):
     print(i)
 
-print("Loop white")
+print("\nLoop white")
 count = 0
 while count < 10:
     print(count)
     count += 1
 
-print("Triangle")
+print("\nTriangle")
 motif = "#"
 for i in range(1, 8):
     print(motif * i)
 
-
 print("\nSquare")
 s_width = 8
 motif = "*"
-for i in range(1,s_width + 1):
+for i in range(1, s_width + 1):
     print(f"{motif} " * s_width)
 
 print("\nMultiplication")
 for i in range(11):
-    print(f"{i} x {i} = {i*i}")
+    print(f"{i} x {i} = {i * i}")
+
+print("\nIterate List")
+skills = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+for skill in skills:
+    print(skill)
+
+print("\nEven numbers")
+for i in range(101):
+    if i % 2 != 0:
+        continue
+    print(i)
+
+print("\nOdd numbers")
+for i in range(101):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+print("\nSum numbers")
+sum_number = 0
+for i in range(101):
+    sum_number += i
+
+print(f"The sum of all numbers is {sum_number}.")
+
+
+print("\nSum even, odd numbers")
+sum_even_number = 0
+sum_odd_number = 0
+for i in range(101):
+    if i%2 == 0:
+        sum_even_number += i
+    else:
+        sum_odd_number += i
+
+print(f"The sum of all evens is {sum_even_number}. And the sum of all odds is {sum_odd_number}.")
+
+print("\nLand countries")
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+]
+for country in countries:
+    if "land" in country:
+        print(country)
+
+print("\nInverse loop")
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+for i in range(len(fruits) - 1, -1, -1):
+    print(fruits[i])
+
+print("\nCount languages")
+languages = []
+for country in countries_list:
+    for language in country["languages"]:
+        languages.append(language)
+print(f"Total languages is {len(languages)}")
