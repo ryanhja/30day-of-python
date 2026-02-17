bc575ed9872f9eda95e60eb9ccc67be14541c99e
ryanhja <rayanstive9@gmail.com>
2026-02-17 21:36:21 +0300

    feat(30DaysOfPython): Add day 19

--- 30DaysOfPython/day_19/file_handling.py
+++ 30DaysOfPython/day_19/file_handling.py
@@ -1,5 +1,18 @@
 # Day 19: 30 Days of python programming
 
+import re
 
-with open("./student.txt", "a") as student:
-    student.write("\nage : 24")
+file_path = r'./obama_speech.txt'
+
+with open(file_path, "r", encoding="utf-8") as f:
+    obama_speech = f.readlines()
+    print(f"Number of lines : {len(obama_speech)}")
+
+    word = 0
+    for row in obama_speech:
+        for w in row:
+            word += 1
+
+    print(obama_speech)
+
+    print(f"Number of words : {word}")
