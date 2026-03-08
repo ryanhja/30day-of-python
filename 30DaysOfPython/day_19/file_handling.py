53348426ee488d67212cc0955c820595458a2e94
ryanhja <rayanstive9@gmail.com>
2026-03-08 21:58:47 +0300

    feat(30DaysOfPython): Add day 19

--- 30DaysOfPython/day_19/file_handling.py
+++ 30DaysOfPython/day_19/file_handling.py
@@ -1,18 +1,36 @@
 # Day 19: 30 Days of python programming
 
 import re
+import json
 
-file_path = r'./obama_speech.txt'
 
-with open(file_path, "r", encoding="utf-8") as f:
-    obama_speech = f.readlines()
-    print(f"Number of lines : {len(obama_speech)}")
+def file_description(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        print(f"File : {file_name}")
+        txt = f.readlines()
+        words = " ".join(txt)
 
-    word = 0
-    for row in obama_speech:
-        for w in row:
-            word += 1
+        print(f"Number of lines : {len(txt)}")
+        print(f"NUmber of words : {len(words.split())}\n")
 
-    print(obama_speech)
 
-    print(f"Number of words : {word}")
+# obama_speech.txt
+file_description('obama_speech.txt')
+
+# michelle_obama_speech.txt
+file_description('michelle_obama_speech.txt')
+
+# donald_speech.txt
+file_description('donald_speech.txt')
+
+# melina_trump_speech.txt
+file_description('melina_trump_speech.txt')
+
+
+# countries_data.json
+with open('countries_data.json', 'r') as f:
+    countries_json = json.loads(f.read())
+
+    countries = [country for country in countries_json]
+
+    print(type(countries))
