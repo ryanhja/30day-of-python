cf2da7e22e0ddb78c60003cc18e276870062b4f4
ryanhja <rayanstive9@gmail.com>
2026-04-04 21:48:31 +0300

    feat(30DaysOfPython): Add day 20

--- 30DaysOfPython/day_20/packaging.py
+++ 30DaysOfPython/day_20/packaging.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 
 print("Romeo and Juliet API")
 romeo_and_juliet = 'http://www.gutenberg.org/files/1112/1112.txt'
@@ -8,6 +9,13 @@ print("Cats API")
 cats_api = 'https://api.thecatapi.com/v1/breeds'
 response_ca = requests.get(cats_api)
 
-weights = [w["weight"]["metric"] for w in response_ca.json()]
+weights = [w["weight"]["metric"].split(" - ") for w in response_ca.json()]
+metrics = [int(weight[1]) - int(weight[0]) for weight in weights]
 
-print(weights)
+min_metric= min(metrics)
+max_metric= max(metrics)
+median_metric= np.median(metrics)
+
+print(min_metric)
+print(max_metric)
+print(median_metric)
