057643dc7b8201c763a6183cbdf20117597f343c
ryanhja <rayanstive9@gmail.com>
2026-04-06 17:59:40 +0300

    feat(30DaysOfPython): Add day 21

--- 30DaysOfPython/day_21/class_and_object.py
+++ 30DaysOfPython/day_21/class_and_object.py
@@ -1,1 +1,16 @@
-# Day 18: 30 Days of python programming
+# Day 21: 30 Days of python programming
+
+class Person:
+    def __init__(self, first_name, last_name=""):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def presentation(self):
+        if len(self.last_name) == 0:
+            return f"First name: {self.first_name}"
+        return f"First name: {self.first_name}, Last name : {self.last_name}"
+
+
+person = Person("Rayan", )
+
+print(person.presentation())
