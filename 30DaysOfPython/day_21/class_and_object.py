5f4f542842f97357c83ba2c7111390cdd98b14e8
ryanhja <rayanstive9@gmail.com>
2026-04-07 20:47:30 +0300

    feat(30DaysOfPython): Add day 21

--- 30DaysOfPython/day_21/class_and_object.py
+++ 30DaysOfPython/day_21/class_and_object.py
@@ -15,10 +15,22 @@ class Statistics:
 
         return sum_data
 
+    def min(self):
+        return min(self.data)
+
+    def max(self):
+        return max(self.data)
+
+    def range(self):
+        return max(self.data) - min(self.data)
+
 
 ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
 
 s = Statistics(ages)
 
-print("Count: ", s.count())
-print("Sum: ", s.sum())
+print("Count:", s.count())
+print("Sum:", s.sum())
+print("Min:", s.min())
+print("Max:", s.max())
+print("Range:", s.range())
