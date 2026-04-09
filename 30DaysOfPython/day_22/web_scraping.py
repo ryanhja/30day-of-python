c557378a2fc0bd12ad1804514af173eb15172d4c
ryanhja <rayanstive9@gmail.com>
2026-04-09 19:07:48 +0300

    feat(30DaysOfPython): Add day 22

--- 30DaysOfPython/day_22/web_scraping.py
+++ 30DaysOfPython/day_22/web_scraping.py
@@ -2,3 +2,18 @@
 
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://www.touteleurope.eu/l-europe-et-moi/la-liste-des-indicatifs-telephoniques-par-pays-de-l-union-europeenne/'
+
+response = requests.get(url)
+
+content = response.content
+
+soup = BeautifulSoup(content, 'html.parser')
+# print(soup.title)
+# print(soup.body.get_text())
+
+with open('phone_indicative_by_country.txt', 'w') as f:
+    f.write(soup.body.get_text())
+
+print("data exported successfully")
