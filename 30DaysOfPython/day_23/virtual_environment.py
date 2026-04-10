773386f9861e08b36ca131d11655a59b76fb1208
ryanhja <rayanstive9@gmail.com>
2026-04-10 21:11:23 +0300

    feat(30DaysOfPython): Add day 22

---
+++ 30DaysOfPython/day_23/virtual_environment.py
@@ -0,0 +1,13 @@
+# Day 23: 30 Days of python programming
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<p>Hello, world</p>"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000, debug=True)
