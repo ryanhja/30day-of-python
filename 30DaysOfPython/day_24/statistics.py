92f0a31866b485574004ef3fd84c69f59c7b0c81
ryanhja <rayanstive9@gmail.com>
2026-04-11 20:39:00 +0300

    feat(30DaysOfPython): Add day 24

--- 30DaysOfPython/day_24/statistics.py
+++ 30DaysOfPython/day_24/statistics.py
@@ -1,1 +1,11 @@
 # Day 24: 30 Days of python programming
+
+import numpy as np
+
+numbers = [21, 45, 71, 32, 29, 78, 56]
+
+print(type(numbers))
+
+array_numbers = np.array(numbers)
+
+print(type(array_numbers))
