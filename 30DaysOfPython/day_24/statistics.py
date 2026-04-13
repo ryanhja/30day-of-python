6610ca0fb922c92ff5f7320343b72669e5bf42e4
ryanhja <rayanstive9@gmail.com>
2026-04-13 21:18:55 +0300

    feat(30DaysOfPython): Add day 24

--- 30DaysOfPython/day_24/statistics.py
+++ 30DaysOfPython/day_24/statistics.py
@@ -2,10 +2,42 @@
 
 import numpy as np
 
-numbers = [21, 45, 71, 32, 29, 78, 56]
+# Python list
+numbers = [21, 2, 3, 4, 5, 6, 7, 8, 9]
+print(type(numbers), ": ", numbers)
 
-print(type(numbers))
+multi_dimensional_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(type(multi_dimensional_numbers), ": ", multi_dimensional_numbers)
 
+# Numpy Array
 array_numbers = np.array(numbers)
+print(type(array_numbers), ": ", array_numbers)
 
-print(type(array_numbers))
+numpy_multi_dimensional = np.array(multi_dimensional_numbers)
+print(type(numpy_multi_dimensional), ": ", numpy_multi_dimensional)
+
+# Numpy Data type
+arrays_float = np.array(numbers, dtype=float)
+print(type(arrays_float), ": ", arrays_float)
+
+# To list
+numpy_to_list = numpy_multi_dimensional.tolist()
+print(numpy_to_list)
+
+# Length
+print(len(numpy_multi_dimensional))
+
+# Shape
+print(numpy_multi_dimensional.shape)
+
+# Size
+print(numpy_multi_dimensional.size)
+
+# Operational
+print(array_numbers + 10)
+print(array_numbers - 10)
+print(array_numbers * 10)
+print(array_numbers / 10)
+print(array_numbers % 10)
+
+#
