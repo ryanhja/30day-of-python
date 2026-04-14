c083449a8027d616b495737673e7f2c79d47b35b
ryanhja <rayanstive9@gmail.com>
2026-04-14 21:15:49 +0300

    feat(30DaysOfPython): Add day 24

--- 30DaysOfPython/day_24/statistics.py
+++ 30DaysOfPython/day_24/statistics.py
@@ -10,34 +10,78 @@ multi_dimensional_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(type(multi_dimensional_numbers), ": ", multi_dimensional_numbers)
 
 # Numpy Array
+print("\nNumpy Array")
 array_numbers = np.array(numbers)
 print(type(array_numbers), ": ", array_numbers)
 
 numpy_multi_dimensional = np.array(multi_dimensional_numbers)
 print(type(numpy_multi_dimensional), ": ", numpy_multi_dimensional)
 
-# Numpy Data type
+# Convert data type
+print("\nConvert data type")
 arrays_float = np.array(numbers, dtype=float)
 print(type(arrays_float), ": ", arrays_float)
 
 # To list
+print("\nConvert to Python List")
 numpy_to_list = numpy_multi_dimensional.tolist()
 print(numpy_to_list)
 
 # Length
+print("\nLength")
 print(len(numpy_multi_dimensional))
 
 # Shape
+print("\nShape")
 print(numpy_multi_dimensional.shape)
 
 # Size
+print("\nSizing")
 print(numpy_multi_dimensional.size)
 
 # Operational
+print("\nOperational")
 print(array_numbers + 10)
 print(array_numbers - 10)
 print(array_numbers * 10)
 print(array_numbers / 10)
 print(array_numbers % 10)
 
-#
+# Numpy data type
+print("\nData Type")
+print(array_numbers.dtype)
+print(arrays_float.dtype)
+
+# Get items
+print("\nGet Items")
+first_row = numpy_multi_dimensional[0]
+second_row = numpy_multi_dimensional[1]
+third_row = numpy_multi_dimensional[2]
+
+print(first_row)
+print(second_row)
+print(third_row)
+
+# Slicing
+print("\nSlicing")
+first_two_row = numpy_multi_dimensional[0:2, 0:3]
+print(first_two_row)
+
+# Reverse data
+print("\nReverse array")
+print(numpy_multi_dimensional[::-1, ::-1])
+
+# Numpy Zeroes
+print("\nNumpy Zeroes")
+numpy_zeros = np.zeros((3, 4), dtype=int)
+print(numpy_zeros)
+
+#Numpy Ones
+print("\nNumpy Ones")
+numpy_ones = np.ones((3,4), dtype=int)
+print(numpy_ones)
+
+#Numpy Twos
+print("\nNumpy Twos")
+numpy_twos = numpy_ones*2
+print(numpy_twos)
