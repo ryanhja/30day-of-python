a4ebb3642fb63bc7373e24b1c3e01119d229e333
ryanhja <rayanstive9@gmail.com>
2026-04-22 22:10:20 +0300

    feat(30DaysOfPython): Add day 25

--- 30DaysOfPython/day_25/python_pandas.py
+++ 30DaysOfPython/day_25/python_pandas.py
@@ -1,7 +1,44 @@
 # Day 25: 30 Days of python programming
 
 import pandas as pd
+import random
 
-df = pd.array((5, 4), dtype=float)
+# Series
+print("\nSeries")
+numbers = [random.randint(1, 100) for _ in range(10)]
+indexes = [i for i in range(1, 11)]
+df = pd.Series(numbers, index=indexes)
 
 print(df)
+
+# Dict
+print("\nDF From Dict")
+dct = {"name": "Rayan", "last name": "Steve"}
+df = pd.Series(dct)
+
+print(df)
+
+# Constant
+print("\nConstant")
+
+df = pd.Series(10, index=indexes)
+print(df)
+
+# List
+print("\nDF From List")
+data = [
+    ['Asabeneh', 'Finland', 'Helsink'],
+    ['David', 'UK', 'London'],
+    ['John', 'Sweden', 'Stockholm']
+]
+df = pd.DataFrame(data, index=[1, 2, 3], columns=['Names', 'Country', 'City'])
+print(df)
+
+# CSV
+print("\nDF From List")
+df = pd.read_csv('weight-height.csv')
+print(df)
+
+# Exploration
+print("\nHead")
+print(df.head())
