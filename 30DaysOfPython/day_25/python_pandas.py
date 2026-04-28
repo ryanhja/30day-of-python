4bd2e3eaee96e77090fb8c9ef19680585238c037
ryanhja <rayanstive9@gmail.com>
2026-04-28 20:07:44 +0300

    feat(30DaysOfPython): Add day 25

--- 30DaysOfPython/day_25/python_pandas.py
+++ 30DaysOfPython/day_25/python_pandas.py
@@ -73,7 +73,7 @@ weight = [45, 78, 62]
 
 data_f["Weight"] = weight
 
-print("\n",data_f)
+print("\n", data_f)
 
 heights = [173, 175, 169]
 data_f['Height'] = heights
@@ -83,14 +83,28 @@ data_f["Weight"] = data_f["Weight"] * 5
 print(data_f)
 
 
-def calculate_bmi ():
+def calculate_bmi():
     weights = data_f['Weight']
     heights = data_f['Height']
     dmi_data = []
-    for w,h in zip(weights, heights):
-        b = w/(h*h)
+    for w, h in zip(weights, heights):
+        b = w / (h * h)
         dmi_data.append(b)
     return dmi_data
 
+
 data_f['BMI'] = calculate_bmi()
-print("\n",data_f)
+print("\n", data_f)
+
+print("\nExercise 1")
+df_hacker_news = pd.read_csv("hacker_news.csv")
+print(df_hacker_news)
+
+print("\nExercise 2")
+print(df_hacker_news.head())
+
+print("\nExercise 3")
+print(df_hacker_news.tail())
+
+print("\nExercise 4")
+print(df_hacker_news["title"])
