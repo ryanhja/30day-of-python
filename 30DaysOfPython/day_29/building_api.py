ac3eef29d132ccf2b5dc13051a6853540118e5a5
ryanhja <rayanstive9@gmail.com>
2026-05-07 20:56:28 +0300

    feat(30DaysOfPython): Add day 29

--- 30DaysOfPython/day_29/building_api.py
+++ 30DaysOfPython/day_29/building_api.py
@@ -1,7 +1,35 @@
 # Day 29: 30 Days of python programming
 
-from flask import Flask,  Response
+from flask import Flask, Response
 import json
-import os
 
 app = Flask(__name__)
+
+
+@app.route('/api/v1/students', methods=['GET'])
+def students():
+    student_list = [
+        {
+            'name': 'Rayan',
+            'country': 'Madagascar',
+            'city': 'Antananarivo',
+            'skills': ['Python', 'Flask', 'DBT', 'Apache Airflow']
+        },
+        {
+            'name': 'David',
+            'country': 'UK',
+            'city': 'London',
+            'skills': ['Python', 'MongoDB']
+        },
+        {
+            'name': 'John',
+            'country': 'Sweden',
+            'city': 'Stockholm',
+            'skills': ['Java', 'C#']
+        }
+    ]
+    return Response(json.dumps(student_list), mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
